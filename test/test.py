45164df66751cc8812479a1de738964943266ad2
HX2003 <HX2003@users.noreply.github.com>
2026-07-13 22:47:42 +0800

    Update verilog test

--- test/test.py
+++ test/test.py
@@ -2,39 +2,35 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import Timer
 
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+    # Not used a clock is not free running
+    # clock = Clock(dut.clk, 10, unit="us")
+    # cocotb.start_soon(clock.start())
 
     # Reset
     dut._log.info("Reset")
+    dut.clk.value = 0
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 0 
+
+    await Timer(100, units="ns")
     dut.rst_n.value = 1
 
+    await Timer(100, units="ns")
+    
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Ensure all mux_sel registers are intialized to zero
+    assert dut.user_project.mux_sel[0].value == 0
+    assert dut.user_project.mux_sel[1].value == 0
+    assert dut.user_project.mux_sel[2].value == 0
+    assert dut.user_project.mux_sel[3].value == 0
+    assert dut.user_project.mux_sel[4].value == 0
